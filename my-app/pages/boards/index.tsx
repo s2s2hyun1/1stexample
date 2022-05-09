2b72e87c8c68829b49e0f1f5db36a2c06bb15362
pre14lim <s2s2hyun0601@gmail.com>
2022-05-09 16:04:31 +0900

    update

--- my-app/pages/boards/index.tsx
+++ my-app/pages/boards/index.tsx
@@ -1,5 +1,6 @@
 import BoardList from "../../src/components/units/board/list/BoardList.container";
 
 export default function BoardListPage() {
-    return <BoardList />;
+    return (<BoardList  />
+    <div>hello</div>);
 }
